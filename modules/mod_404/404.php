c8690f7eb98c09d1c76f70f97c9b340e5fb0806b
Riyanto <riyanto.droider@gmail.com>
2023-09-08 23:50:41 +0700

    Pembuatan modul 404 (not found)

---
+++ modules/mod_404/404.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+            <h1>404</h1>
+            <p>Halaman tidak ditemukan!</p>
+        </div>
+    </div>
+</div>
